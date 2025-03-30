429e879610cb3eea3436fc13a49af2b95f4f5cb0
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-03-30 13:42:15 -0400

    Task 2 - Created SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -17,12 +17,42 @@ class Customer {
     // adds up all of the purchase amount and returns the total amount
     getTotalSpent() {
         const totalAmount = this.purchaseHistory.reduce((sum, purchase) => sum + purchase, 0);
-        console.log(`Total Amount Spent: $${totalAmount}`);
+        //console.log(`Total Amount Spent: $${totalAmount}`);
         return totalAmount;
+    };
+};
+
+// Task 2: Create a SalesRep Class
+
+// creates a class that will make a client list and give the total amount spent from each client
+class SalesRep {
+    constructor(name) {
+        this.name = name;
+        this.clients = [];
+    };
+
+    // uses a method to add clients to the array
+    addClient(customer) {
+        this.clients.push(customer);
+    };
+
+    // uses a method to get
+    getClientTotal(name) {
+        // uses find to find the exact name
+        const clientName = this.clients.find(client => client.name === name);
+
+        // uses previous methods and logs the output in the console
+        const totalSpentByClient = clientName.getTotalSpent();
+        console.log(`Total spent by ${name}: $${totalSpentByClient}`);
     };
 };
 
 // Task 1 - test data
 const customer1 = new Customer('Cade Cunningham', 'ccunningham@gmail.com');
 customer1.addPurchase(15);
-customer1.getTotalSpent();
+console.log(`Total Amount Spent: $${customer1.getTotalSpent()}`);
+
+// Task 2 - test data
+const salesRep1 = new SalesRep('Travis Hunter');
+salesRep1.addClient(customer1);
+salesRep1.getClientTotal('Cade Cunningham');
