6780372ffaf6be3936cbf92ec4f2fb4004331bdc
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-03-30 21:28:33 -0400

    Adding Comments and Cleaning up the Code

--- cc_17.js
+++ cc_17.js
@@ -18,7 +18,7 @@ class Customer {
     // adds up all of the purchase amount and returns the total amount
     getTotalSpent() {
         const totalAmount = this.purchaseHistory.reduce((sum, purchase) => sum + purchase, 0);
-        //console.log(`Total Amount Spent: $${totalAmount}`);
+        
         return totalAmount;
     };
 };
@@ -43,22 +43,24 @@ class SalesRep {
         // uses find to find the exact name
         const clientName = this.clients.find(client => client.name === name);
 
-        // uses previous methods and logs the output in the console
-        // const totalSpentByClient = clientName.getTotalSpent();
-        // console.log(`Total spent by ${name}: $${totalSpentByClient}`);
+        // uses if statement and previous methods then logs the output in the console
         if (clientName) {
             const totalSpentByClient = clientName.getTotalSpent();
             console.log(`Total spent by ${name}: $${totalSpentByClient.toFixed(2)}`);
         }
     };
 
+    // creates method to make a report of the clients
     reportOfClients() {
+        // creates a variable, assigns value of total revenue, then logs in console
         const totalRev = this.clients.reduce((sum, client) => sum + client.getTotalSpent(), 0);
         console.log(`Total revenue from all of the clients: $${totalRev.toFixed(2)}`);
 
+        // big spending clients variable and log
         const bigSpendingClients = this.clients.filter(client => client.getTotalSpent() > 500);
         console.log('High Rollers:', bigSpendingClients.map(client => client.name));
 
+        // creates a client summary and logs it in the console
         const customerSummary = this.clients.map(client => ({
             name: client.name,
             email: client.email,
@@ -68,23 +70,28 @@ class SalesRep {
     }
 };
 
-// Task 3 - 
+
+// Task 3: Create a VIPCustomer Class (extends Customer)
+
+// creates a vip customer class that extends properties from the customer class
 class VIPCustomer extends Customer {
-    constructor(name, email, vipLevel) {
+    // takes customer class features and adds vip level that can only be gold or platinum
+    constructor(name, email, vipLevel=["Gold","Platinum"]) {
         super(name, email);
         this.vipLevel = vipLevel;
     };
 
+    // creates a method in the class that calculates the VIP customer bonus
     getTotalSpent() {
         const total = super.getTotalSpent();
         const totalWithBonus = total * 1.1;
 
-        //console.log(`Total Spent by VIP With Loyalty Bonus: $${totalWithBonus.toFixed(0)}`);
         return totalWithBonus;
     };
 }
 
 
+// Test Data (below)
 // Task 1 - test data
 const customer1 = new Customer('Cade Cunningham', 'ccunningham@gmail.com');
 customer1.addPurchase(15);
@@ -104,13 +111,13 @@ console.log(`Total Amount Spent: $${customer3.getTotalSpent()}`);
 const vipCustomer1 = new VIPCustomer('Lebron James', 'bronbron24@gmail.com', 'Gold');
 vipCustomer1.addPurchase(2000);
 console.log(`Total Spent by VIP With Loyalty Bonus: $${vipCustomer1.getTotalSpent().toFixed(2)}`);
-vipCustomer1.getTotalSpent();
+
 
 const vipCustomer2 = new VIPCustomer('Jameis Winston', 'winstonj22@gmail.com', 'Platinum');
 vipCustomer2.addPurchase(1500);
 vipCustomer2.addPurchase(250);
 console.log(`Total Spent by VIP With Loyalty Bonus: $${vipCustomer2.getTotalSpent().toFixed(2)}`);
-//vipCustomer2.getTotalSpent();
+
 
 // Task 2 - test data
 const salesRep1 = new SalesRep('Travis Hunter');
